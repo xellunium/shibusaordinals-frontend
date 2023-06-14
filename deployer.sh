0cdad4975a7dc1ba816efa26a6c3b37ac5d3b42d
shamir0xe <shamir0xe@gmail.com>
2023-06-14 18:15:21 +0330

    fix: fixed deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -10,13 +10,6 @@ echo 'Cloning repository'
 [ -d "$release_dir" ] || mkdir -p "$release_dir"
 git clone --depth 1 $repository "$new_release_dir" --recursive
 
-echo 'Running npm build'
-npm run build
-
-echo 'Linking build folder'
-rm -rf "$new_release_dir"/build
-ln -nfs "$app_dir"/build "$new_release_dir"/build
-
 echo 'Linking NodeModules'
 rm -rf "$new_release_dir"/node_modules
 ln -nfs "$app_dir"/node_modules "$new_release_dir"/node_modules
@@ -25,12 +18,16 @@ echo 'Linking Assets'
 rm -rf "$new_release_dir"/assets
 ln -nfs "$app_dir"/assets "$new_release_dir"/assets
 
-# echo 'Linking ENV'
-# rm -rf "$new_release_dir"/configs/env.json
-# ln -sfn "$app_dir"/configs/env.json "$new_release_dir"/configs/env.json
-
 echo 'Unlinking previous release'
 unlink "$output_dir"/current
 
 echo 'Linking current release'
 ln -nfs "$new_release_dir" "$output_dir"/current
+
+echo 'Installing dependencies'
+cd "$new_release_dir" || exit
+npm install
+
+echo 'Running npm build'
+cd "$new_release_dir" || exit
+npm run build
