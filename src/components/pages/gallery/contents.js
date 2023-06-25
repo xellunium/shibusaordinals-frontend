ccca7117f3f2f136df52f3876b9d99c8534c14a7
shamir0xe <shamir0xe@gmail.com>
2023-06-26 03:03:05 +0330

    style: revert the gallery format from webp to jpg

--- src/components/pages/gallery/contents.js
+++ src/components/pages/gallery/contents.js
@@ -3,19 +3,19 @@ import { imgPack } from "src/helpers/imgs/index";
 const contents = {
     imgs: {
         gallery: [
-            imgPack("gallery/1.webp", "1"),
-            imgPack("gallery/2.webp", "2"),
-            imgPack("gallery/3.webp", "3"),
-            imgPack("gallery/4.webp", "4"),
-            imgPack("gallery/5.webp", "5"),
-            imgPack("gallery/6.webp", "6"),
-            imgPack("gallery/7.webp", "7"),
-            imgPack("gallery/8.webp", "8"),
-            imgPack("gallery/9.webp", "9"),
-            imgPack("gallery/10.webp", "10"),
-            imgPack("gallery/11.webp", "11"),
-            imgPack("gallery/12.webp", "12"),
-            imgPack("gallery/13.webp", "13"),
+            imgPack("gallery/1.jpg", "1"),
+            imgPack("gallery/2.jpg", "2"),
+            imgPack("gallery/3.jpg", "3"),
+            imgPack("gallery/4.jpg", "4"),
+            imgPack("gallery/5.jpg", "5"),
+            imgPack("gallery/6.jpg", "6"),
+            imgPack("gallery/7.jpg", "7"),
+            imgPack("gallery/8.jpg", "8"),
+            imgPack("gallery/9.jpg", "9"),
+            imgPack("gallery/10.jpg", "10"),
+            imgPack("gallery/11.jpg", "11"),
+            imgPack("gallery/12.jpg", "12"),
+            imgPack("gallery/13.jpg", "13"),
         ],
         background: imgPack("backgrounds/gallery.webp", "gallery-bg")
     }
